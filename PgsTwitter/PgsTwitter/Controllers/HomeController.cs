22e750dbf32b99b790f76c157d97eb2c24f52085
Przemysław Izdebski <przemek.izdebski@gmail.com>
2014-10-25 11:03:56 +0200

    basic app

---
+++ PgsTwitter/PgsTwitter/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PgsTwitter.Models.Home;
+
+namespace PgsTwitter.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            var model = new Message() {Username = "Zenek"};
+            return View(new List<Message>(){model});
+        }
+
+    }
+}
---
+++ PgsTwitter/PgsTwitter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PgsTwitter.MvcApplication" Language="C#" %>
